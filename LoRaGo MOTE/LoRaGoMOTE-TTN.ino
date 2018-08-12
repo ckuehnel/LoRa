e421a3e0d4ea7896d68a87128f6a91e9e4b06566
Claus Kühnel <info@ckuehnel.ch>
2018-08-12 16:22:56 +0200

    Add files via upload

---
+++ LoRaGo MOTE/LoRaGoMOTE-TTN.ino
@@ -0,0 +1,137 @@
+/*
+ * Author: Peng Wei
+ * Date: 2018-01-21
+ *
+ * Transmit a one byte packet via TTN. This happens as fast as possible, while still keeping to
+ * the 1% duty cycle rules enforced by the RN2483's built in LoRaWAN stack. Even though this is
+ * allowed by the radio regulations of the 868MHz band, the fair use policy of TTN may prohibit this.
+ *
+ * CHECK THE RULES BEFORE USING THIS PROGRAM!
+ *
+ * CHANGE ADDRESS!
+ * Change the device address, network (session) key, and app (session) key to the values
+ * that are registered via the TTN dashboard.
+ * The appropriate line is "myLora.initABP(XXX);" or "myLora.initOTAA(XXX);"
+ * When using ABP, it is advised to enable "relax frame count".
+ *
+ * Remember that the serial port should be initialised before calling initTTN().
+ * If you use 57600 baud, you can remove the line "myLora.autobaud();".
+ * 
+ * Small modifications: Claus Kühnel info@ckuehnel.ch 2018-08-12
+ *
+ */
+#include <rn2xx3.h>
+#include <SoftwareSerial.h>
+
+#define DEBUG 1
+
+static uint8_t mydata[] = "               ";
+
+SoftwareSerial mySerial(2, 3); // RX, TX
+
+//create an instance of the rn2xx3 library,
+//giving the software serial as port to use
+rn2xx3 myLora(mySerial);
+
+// the setup routine runs once when you press reset:
+void setup()
+{
+  //output LED pin
+  pinMode(13, OUTPUT);
+  led_on();
+
+  // Open serial communications and wait for port to open:
+  Serial.begin(57600); //serial port to computer
+  mySerial.begin(9600); //serial port to radio
+  Serial.println("Startup");
+
+  initialize_radio();
+
+  //transmit a startup message
+  myLora.tx("TTN Mapper on TTN Enschede node");
+
+  led_off();
+  delay(2000);
+}
+
+void initialize_radio()
+{
+  delay(100); //wait for the RN2xx3's startup message
+  mySerial.flush();
+
+  //Autobaud the rn2483 module to 9600. The default would otherwise be 57600.
+  myLora.autobaud();
+
+  //check communication with radio
+  String hweui = myLora.hweui();
+  while(hweui.length() != 16)
+  {
+    Serial.println("Communication with RN2xx3 unsuccessful. Power cycle the board.");
+    Serial.println(hweui);
+    delay(10000);
+    hweui = myLora.hweui();
+  }
+
+  //print out the HWEUI so that we can register it via ttnctl
+  Serial.println("When using OTAA, register this DevEUI: ");
+  Serial.println(myLora.hweui());
+  Serial.println("RN2xx3 firmware version:");
+  Serial.println(myLora.sysver());
+
+  //configure your keys and join the network
+  Serial.println("Trying to join TTN");
+  bool join_result = false;
+
+
+  /*
+   * ABP: initABP(String addr, String AppSKey, String NwkSKey);
+   * Paste the example code from the TTN console here:
+   */
+  const char *devAddr = "260111F1";
+  const char *nwkSKey = "CF8D3A8F2E675DBC97575F8AAE7BEF48";
+  const char *appSKey = "E8A496D62F2E8D5303874B65499D7FFA";
+
+  join_result = myLora.initABP(devAddr, appSKey, nwkSKey);
+
+  /*
+   * OTAA: initOTAA(String AppEUI, String AppKey);
+   * If you are using OTAA, paste the example code from the TTN console here:
+   */
+  //const char *appEui = "70B3D57ED00001A6";
+  //const char *appKey = "A23C96EE13804963F8C2BD6285448198";
+
+  //join_result = myLora.initOTAA(appEui, appKey);
+
+
+  while(!join_result)
+  {
+    Serial.println("Unable to join. Are your keys correct, and do you have TTN coverage?");
+    delay(60000); //delay a minute before retry
+    join_result = myLora.init();
+  }
+  Serial.println("Successfully joined TTN");
+
+}
+
+// the loop routine runs over and over again forever:
+void loop()
+{
+    led_on();
+
+    if (!DEBUG) Serial.println("TXing");
+    getValue();
+    myLora.tx(mydata);    // message from getValue()
+
+    led_off();
+    delay(5000);
+}
+
+void led_on()
+{
+  digitalWrite(13, 1);
+}
+
+void led_off()
+{
+  digitalWrite(13, 0);
+}
